512f1fbe6d7d25845047a8af071d4cd3370bd588
victor <vgarzom@gmail.com>
2019-09-14 17:27:57 -0500

    fixes the problem with the modal-body

--- cucumber/features/step-definitions/index.js
+++ cucumber/features/step-definitions/index.js
@@ -1,1 +1,41 @@
 //Complete siguiendo las instrucciones del taller
+var { defineSupportCode } = require('cucumber');
+var { expect } = require('chai');
+
+defineSupportCode(({ Given, When, Then }) => {
+  Given('I go to losestudiantes home screen', () => {
+    browser.url('/');
+    if (browser.isVisible('button=Cerrar')) {
+      browser.click('button=Cerrar');
+    }
+  });
+
+  When('I open the login screen', () => {
+    browser.waitUntil(() => {
+      return !browser.isExisting('modal-body');
+    });
+    browser.waitForVisible('button=Ingresar', 5000);
+    browser.click('button=Ingresar');
+  });
+
+  When('I fill a wrong email and password', () => {
+    var cajaLogIn = browser.element('.cajaLogIn');
+
+    var mailInput = cajaLogIn.element('input[name="correo"]');
+    mailInput.click();
+    mailInput.keys('wrongemail@example.com');
+
+    var passwordInput = cajaLogIn.element('input[name="password"]');
+    passwordInput.click();
+    passwordInput.keys('123467891')
+  });
+
+  When('I try to login', () => {
+    var cajaLogIn = browser.element('.cajaLogIn');
+    cajaLogIn.element('button=Ingresar').click()
+  });
+
+  Then('I expect to not be able to login', () => {
+    browser.waitForVisible('.aviso.alert.alert-danger', 5000);
+  });
+});
